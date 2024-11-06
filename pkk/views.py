a1431e24afd6f8e3987e50fac938efb599434edb
Stas <shishkinstas@mail.ru>
2024-11-06 19:28:57 +0700

    6

--- pkk/views.py
+++ pkk/views.py
@@ -10,6 +10,14 @@ from django.views.generic import TemplateView
 api_client = PKKRosreestrAPIClient()
 
 
+def _strip_cadastral_id(cadastral_id):
+    stripped_cadastral_id = []
+    cadastral_id = cadastral_id.split(':')
+    for part in cadastral_id:
+        if part:
+            stripped_cadastral_id.append(part[:-1].lstrip('0') + part[-1])
+    return ':'.join(stripped_cadastral_id)
+
 def home(request):
     return render(request,'pkk/home.html')
 
@@ -18,16 +26,26 @@ def pkk(request):
     if request.method == 'POST':
         api_client = PKKRosreestrAPIClient()
         cadastral_id = request.POST.get("build_kad_num")
-        print(cadastral_id)
+        build_kad_num = json.loads(request.body)
+        print(build_kad_num)
         search_params = {
-            'cadastral_id': cadastral_id, 'limit': 1, 'tolerance': 170}
+            'cadastral_id': build_kad_num['new_kadnum'], 'limit': 10, 'tolerance': 170}
         url = api_client.SEARCH_BUILDING_BY_CADASTRAL_ID_URL.format(**search_params)
+        url1 = api_client.SEARCH_PARCEL_BY_CADASTRAL_ID_URL.format(**search_params)
         CONTENT_TYPE_JSON = 'application/json'
+
         print(url)
+        a = _strip_cadastral_id(build_kad_num['new_kadnum'])
+        print(a)
 
         data = api_client.get_building_by_cadastral_id(**search_params)
-        print(data)
-        dd = {'name': 'name', 'email': 'email', 'subject': 'subject'}
+        data1 = api_client.get_parcel_by_cadastral_id(**search_params)
+        print(data['total'])
+        print(data1['total'])
+        print(data1)
+        if data['total'] == 1:
+            return JsonResponse(data, safe=False)
+        return JsonResponse(data1, safe=False)
 
        # print(f'{PKKRosreestrAPIClient.BASE_URL}/features/1/{cadastral_id}')
        # url = f'{PKKRosreestrAPIClient.BASE_URL}/features/1/?text={cadastral_id}&tolerance=1&limit=11'
@@ -36,7 +54,7 @@ def pkk(request):
        # response = requests.request("GET", url, headers=headers, verify=False)
        # data = json.loads(response.text)
        # print(data['feature']['attrs']['address'])
-        return JsonResponse(dd, safe=False)
+
     return render(request, 'pkk/pkk.html')
 
 def pk(request):
